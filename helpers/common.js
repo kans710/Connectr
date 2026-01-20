5ba18973009c712a0a2dcac3da87d98f75896a95
Karan Singh <singhkans488@gmail.com>
2026-01-20 18:15:08 +0530

    basic setup

---
+++ helpers/common.js
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+const{width: deviceWidth , height: deviceHeight}=Dimensions.get('window'); //destructuring
+const hp = percentage=>{
+    return(percentage*deviceHeight)/100;
+}
+const wp = percentage=>{
+    return(percentage*deviceWidth)/100;
+}
